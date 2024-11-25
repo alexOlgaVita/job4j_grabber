3657d4151d2629e49d77c026d5fc80f80ba31f33
olga.ilyina <olga.ilyina@axenix.pro>
2024-11-25 16:27:18 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/grabber/model/Post.java
@@ -0,0 +1,57 @@
+package grabber.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Post {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 17;
+        result = prime * result + ((link == null) ? 0 : link.hashCode());
+        result = prime * result + id;
+        result = prime * result + created.truncatedTo(ChronoUnit.MILLIS).hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Post item = (Post) obj;
+        return (id == item.id) && (title.equals(item.title))
+                && (link.equals(item.link)) && (description.equals(item.description))
+                && (created.truncatedTo(ChronoUnit.MILLIS).equals(item.created.truncatedTo(ChronoUnit.MILLIS)));
+    }
+}
